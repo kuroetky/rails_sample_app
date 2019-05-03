85f6cdfda21657d9d5e557fb0b22a34c0bcc7393
Kuroe Takuya <a1911tk@aiit.ac.jp>
2019-05-03 13:27:58 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  #GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    #=> app/veiws/users/show.html.erb
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      #failure
+      render 'new'
+    end
+  end
+  
+  private
+    #パラメータチェック関数
+    def user_params
+      params.require(:user).permit(
+        :name, :email, :password,
+        :password_confirmation)
+    end
 end
