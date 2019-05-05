d30ccdd59686f8b301785cd493a8594cf8e3d8d6
Kuroe Takuya <a1911tk@aiit.ac.jp>
2019-05-05 04:56:05 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #success
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       #failure
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   
   # DELETE /logout
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
